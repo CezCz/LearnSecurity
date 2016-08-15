b9405ec36226bfcee5b0a3ea04f4e24f245c9106
Cezary Czernecki <czarek.czernecki@gmail.com>
2016-08-15 17:19:54 +0200

    model change and admin panel register

--- LearnSecurity/admin.py
+++ LearnSecurity/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from LearnSecurity import models
+
+admin.site.register(models.Level)
+admin.site.register(models.Maze)
+
 # Register your models here.
--- LearnSecurity/models.py
+++ LearnSecurity/models.py
@@ -4,17 +4,24 @@ from django.contrib.postgres.fields import JSONField
 
 class Maze(models.Model):
     name = models.CharField(max_length=30)
+    desc = models.CharField(max_length=30)
     difficulty = models.IntegerField()
-    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + " Maze"
 
 
 class Level(models.Model):
+    maze = models.ForeignKey(Maze)
     level = models.IntegerField()
-    password = models.CharField(max_length=64)
-    password_next = models.CharField(max_length=64)
-    lecture_id = models.CharField(max_length=5)
-    description = models.CharField(max_length=200)
-    riddle = JSONField()
+    # password = models.CharField(max_length=64)
+    # password_next = models.CharField(max_length=64)
+    short_description = models.CharField(max_length=200)
+    program_description = JSONField()
+    # riddle = JSONField()
+
+    def __str__(self):
+        return str(self.maze) + ", Level: " + str(self.level)
 
 # Todo
 # class UserProgress
