b0ca67a1b5b6d08cee5df6c885db91d43c22dab0
Cezary Czernecki <czarek.czernecki@gmail.com>
2016-10-04 20:04:02 +0200

    tweak site properties, add user progress tracking and more

--- LearnSecurity/views.py
+++ LearnSecurity/views.py
@@ -2,13 +2,13 @@ import json
 import operator
 
 from django.contrib.auth import logout, authenticate, login
-from django.contrib.auth.forms import SetPasswordForm
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse, HttpResponseServerError
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.template.context_processors import csrf
 
 from LearnSecurity.forms import RegistrationForm, LoginForm, EditUserForm, ChangePasswordForm, ChangePicForm
-from LearnSecurity.models import Maze, Level, LevelStep, UserPic
+from LearnSecurity.models import Maze, Level, LevelStep, UserPic, UserProgress
 
 
 def index(request):
@@ -19,38 +19,56 @@ def index(request):
         return render(request, "base.html", {"mazes": mazes, "login_form": LoginForm()})
 
 
-def basic_linux_info_handler(request, mazeName):
-    maze = Maze.objects.get(name=mazeName)
+def basic_linux_info_handler(request, maze_name):
+    maze = Maze.objects.get(name=maze_name)
     levels = list(Level.objects.filter(maze=maze))
     levels.sort(key=operator.attrgetter('level'))
-    return render_to_response("mazelevelcards.html", {"maze": maze, "levels": levels})
+    return render(request, "mazelevelcards.html", {"maze": maze, "levels": levels})
 
 
-def basic_linux_handler(request, mazeName, level):
-    maze = Maze.objects.get(name=mazeName)
+def basic_linux_handler(request, maze_name, level):
+    maze = Maze.objects.get(name=maze_name)
     db_level = Level.objects.get(maze=maze, level=level)
     steps = list(LevelStep.objects.filter(level=db_level))
     steps = sorted(steps, key=operator.attrgetter('level_step'))
-    return render_to_response("basiclinux.html", {"maze": maze, "level": db_level, "steps": steps})
+
+    passed_level_steps = []
+    if request.user.is_authenticated:
+        passed_steps = UserProgress.objects.filter(level_step__in=steps, user=request.user)
+        passed_level_steps = [passed['level_step'] for passed in
+                              LevelStep.objects.filter(userprogress__in=passed_steps).values('level_step')]
+
+    return render(request, "basiclinux.html",
+                  {"maze": maze, "level": db_level, "steps": steps, "passed_steps": passed_level_steps})
 
 
-def level_step_help(request, mazeName, level, step):
-    maze = Maze.objects.get(name=mazeName)
+@login_required
+def level_step_help(request, maze_name, level, step):
+    maze = Maze.objects.get(name=maze_name)
     db_level = Level.objects.get(maze=maze, level=level)
     step = LevelStep.objects.get(level=db_level, level_step=step)
     return JsonResponse({'help': step.help})
 
 
-def level_step_check_password(request, mazeName, level, step):
-    maze = Maze.objects.get(name=mazeName)
+def level_step_check_password(request, maze_name, level, step):
+    maze = Maze.objects.get(name=maze_name)
     db_level = Level.objects.get(maze=maze, level=level)
     step = LevelStep.objects.get(level=db_level, level_step=step)
     answer = step.password == request.POST["answer"]
+    if answer is True and request.user.is_authenticated:
+        try:
+            user_progress = UserProgress.objects.get(level_step=step, user=request.user)
+            user_progress.passed = answer
+            user_progress.save()
+        except UserProgress.DoesNotExist:
+            UserProgress(level_step=step, user=request.user, passed=answer).save()
+
     return JsonResponse({'check': answer})
 
 
-def level_step_peek_password(request, mazeName, level, step):
-    maze = Maze.objects.get(name=mazeName)
+@login_required
+def level_step_peek_password(request, maze_name, level, step):
+    maze = Maze.objects.get(name=maze_name)
     db_level = Level.objects.get(maze=maze, level=level)
     step = LevelStep.objects.get(level=db_level, level_step=step)
     return JsonResponse({'peek': step.password})
@@ -72,6 +90,7 @@ def log_in(request):
     return return_val
 
 
+@login_required
 def log_out(request):
     logout(request)
     if request.user.is_authenticated:
@@ -92,9 +111,10 @@ def register(request):
     else:
         token = {'form': RegistrationForm()}
         token.update(csrf(request))
-        return render_to_response("register.html", token)
+        return render(request, "register.html", token)
 
 
+@login_required
 def change_user_creds(request):
     if request.method == 'POST':
         form = EditUserForm(request.POST, instance=request.user)
@@ -111,13 +131,14 @@ def change_user_creds(request):
     else:
         token = {'edit_user_form': EditUserForm(instance=request.user), "change_pic": ChangePicForm()}
         token.update(csrf(request))
-        return render_to_response("changeuser.html", token)
+        return render(request, "changeuser.html", token)
 
 
 def login_component(request):
     return render(request, 'logincomponent.html')
 
 
+@login_required
 def change_user_password(request):
     if request.method == 'POST':
         form = ChangePasswordForm(data=request.POST, user=request.user)
@@ -129,4 +150,4 @@ def change_user_password(request):
     else:
         token = {'change_password_form': ChangePasswordForm(user=request.user)}
         token.update(csrf(request))
-        return render_to_response("changepassword.html", token)
+        return render(request, "changepassword.html", token)
--- LearnSecurityApp/urls.py
+++ LearnSecurityApp/urls.py
@@ -21,15 +21,14 @@ from LearnSecurity import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^Maze/(?P<mazeName>[a-zA-Z0-9]+)$', views.basic_linux_info_handler),
-    url(r'^Maze/(?P<mazeName>[a-zA-Z0-9]+)/(?P<level>[0-9][0-9]?)$', views.basic_linux_handler),
-    url(r'^Maze/(?P<mazeName>[a-zA-Z0-9]+)/(?P<level>[0-9][0-9]?)/help/(?P<step>[0-9])$', views.level_step_help),
-    url(r'^Maze/(?P<mazeName>[a-zA-Z0-9]+)/(?P<level>[0-9][0-9]?)/check/(?P<step>[0-9])$',
+    url(r'^Maze/(?P<maze_name>[a-zA-Z0-9]+)$', views.basic_linux_info_handler),
+    url(r'^Maze/(?P<maze_name>[a-zA-Z0-9]+)/(?P<level>[0-9][0-9]?)$', views.basic_linux_handler),
+    url(r'^Maze/(?P<maze_name>[a-zA-Z0-9]+)/(?P<level>[0-9][0-9]?)/help/(?P<step>[0-9])$', views.level_step_help),
+    url(r'^Maze/(?P<maze_name>[a-zA-Z0-9]+)/(?P<level>[0-9][0-9]?)/check/(?P<step>[0-9])$',
         views.level_step_check_password),
-    url(r'^Maze/(?P<mazeName>[a-zA-Z0-9]+)/(?P<level>[0-9][0-9]?)/peek/(?P<step>[0-9])$',
+    url(r'^Maze/(?P<maze_name>[a-zA-Z0-9]+)/(?P<level>[0-9][0-9]?)/peek/(?P<step>[0-9])$',
         views.level_step_peek_password),
     url(r'^admin/', admin.site.urls),
-
     url(r'^login/', views.log_in),
     url(r'^logout/', views.log_out),
     url(r'^register/', views.register),
